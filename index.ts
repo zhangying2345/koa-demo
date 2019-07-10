cd6fa5775f538c608cb54ca1724ae95758e27e37
zhangYing <zy49666@ly.com>
2019-07-10 22:30:10 +0800

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
-const routes = require('./router.ts');
+import { News } from './src/news/news';
+// const routes = require('./router.ts');
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
-app.use(routes.routes());
+const router = new Router();
+const news = new News();
+
+router.get('/', async (ctx, next) => {
+    const newsBaiDu = await news.getNews();
+    ctx.body = newsBaiDu;
+});
+
+app.use(router.routes());
 app.listen(3000);
--- src/news/news.ts
+++ src/news/news.ts
@@ -1,23 +1,35 @@
 //test
 var http=require("http");
 var fs=require("fs");
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+export class News {
+
+    getNews() {
+        const url = "http://news.baidu.com/"; //网址
+        var strHtml="";
+        var results=[];
+        
+        return fetch(url)
+        .then(res => res.text())
+        // .then(body => console.log(body));
+        // http.get(url, res => {
+        //     res.on("data",function(chunk){
+        //         strHtml+=chunk;
+        //     })
+        //     res.on("end",function(){
+        
+        //         console.log(strHtml);
+        //         var $=cheerio.load(strHtml);
+        
+        //         $("#channel-all li").each((iten,i)=>{
+        //             console.log($(i).text());
+        //             results.push($(i).text());
+        //         })
+        //     });
+        // })
+    }
+}
 
 
-const url = "http://news.baidu.com/"; //网址
-
-var strHtml="";
-var results=[];
-http.get(url, function(res){
-    res.on("data",function(chunk){
-        strHtml+=chunk;
-    })
-    res.on("end",function(){
-
-        console.log(strHtml);
-        var $=cheerio.load(strHtml);
-
-        $("#channel-all li").each((iten,i)=>{
-            console.log($(i).text());
-        })
-    });
-})
